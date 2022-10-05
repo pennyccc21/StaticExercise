4354520ab5517b14ecf2f08ffbade3108f6b62d8
Peter luque <Peterluque21@gmail.com>
2022-10-05 13:11:48 -0400

    commit

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -6,6 +6,7 @@ namespace StaticExercise
     {
         static void Main(string[] args)
         {
+           
             var celsius = TempConverter.FahrenheitToCelsius(68); // 20
             var fahrenheit = TempConverter.CelsiusToFahrenheit(20); //68
 
